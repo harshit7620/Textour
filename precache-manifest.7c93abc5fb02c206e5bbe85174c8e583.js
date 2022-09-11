8735423bb4f751ec38405677b8f71df3e03798fc
Harsh <harshitsoni7620@gmail.com>
2022-09-11 15:55:00 +0530

    Updates

---
+++ precache-manifest.7c93abc5fb02c206e5bbe85174c8e583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8acf8e2d6c627afe307",
+    "url": "/Textour/static/css/main.607da88c.chunk.css"
+  },
+  {
+    "revision": "f8acf8e2d6c627afe307",
+    "url": "/Textour/static/js/main.f8acf8e2.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Textour/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Textour/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "c5e41a151cde0ccc858e",
+    "url": "/Textour/static/js/runtime~main.c5e41a15.js"
+  },
+  {
+    "revision": "7408da639e53b666e1299f87ec6829a5",
+    "url": "/Textour/index.html"
+  }
+];
